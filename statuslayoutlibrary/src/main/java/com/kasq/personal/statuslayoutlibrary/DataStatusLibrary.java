d80b80c1f1ce45ee22e71498bf826012d5291c60
kasq <462990703@qq.com>
2018-08-21 18:00:17 +0800

    创建项目

---
+++ statuslayoutlibrary/src/main/java/com/kasq/personal/statuslayoutlibrary/DataStatusLibrary.java
@@ -0,0 +1,23 @@
+package com.kasq.personal.statuslayoutlibrary;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class DataStatusLibrary extends FrameLayout {
+    public DataStatusLibrary(@NonNull Context context) {
+        super(context);
+    }
+
+    public DataStatusLibrary(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DataStatusLibrary(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
